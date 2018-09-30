6456c015ffad40eac58ca69ebe4f9a456ce3b0cc
lhc9138 <lhc9138@naver.com>
2018-09-30 22:01:38 +0900

    Add method signature
    insert, print1, print2, remove

--- ScoreManagement/src/com/scoremanagement/persistance/CourseDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/CourseDAO.java
@@ -1,5 +1,38 @@
 package com.scoremanagement.persistance;
 
-public class CourseDAO {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.scoremanagement.domain.Course;
+
+public class CourseDAO {
+	
+	// 과정 입력 메소드
+	public int insert(Course c) {
+		int result = 0;
+		
+		return result;
+	}
+	// 과정 출력 메소드
+	// 과정번호 / 과정이름
+	public List<Course> print1() {
+		List<Course> list = new ArrayList<Course>();
+		
+		return list;
+	}
+	
+	// 과정 출력 메소드
+	// 과정번호 / 과정이름 / 삭제가능여부
+	public List<Course> print2() {
+		List<Course> list = new ArrayList<Course>();
+		
+		return list;
+	}
+	
+	// 과정 삭제 메소드
+	public int remove(Course c) {
+		int result = 0;
+		
+		return result;
+	}
 }
