d93ed256cffb1c27291837c7c95983edef169ca3
lhc9138 <lhc9138@naver.com>
2018-09-30 14:47:33 +0900

    Create Field, Constructor, Setter, Getter, printMethod

--- ScoreManagement/src/com/scoremanagement/domain/Course.java
+++ ScoreManagement/src/com/scoremanagement/domain/Course.java
@@ -1,5 +1,59 @@
 package com.scoremanagement.domain;
 
 public class Course {
+	// Field
+	private String course_id;
+	private String course_name;
+	private int count_;
 	
+	// Constructor
+	public Course() {
+	
+	}
+
+	public Course(String course_id, String course_name) {
+		this.course_id = course_id;
+		this.course_name = course_name;
+	}
+	
+	public Course(String course_id, String course_name, int count_) {
+		this.course_id = course_id;
+		this.course_name = course_name;
+		this.count_ = count_;
+	}
+
+	// Getter
+	public String getCourse_id() {
+		return course_id;
+	}
+
+	public String getCourse_name() {
+		return course_name;
+	}
+
+	public int getCount_() {
+		return count_;
+	}
+
+	// Setter
+	public void setCourse_name(String course_name) {
+		this.course_name = course_name;
+	}
+
+	// print method
+	// Return 과목번호 / 과목이름
+	public String print1() {
+		String result = "";
+		result = String.format("%s / %s", 
+				this.getCourse_id(), this.getCourse_name());
+		return result;
+	}
+
+	// Return 과목번호 / 과목이름 / 삭제가능여부
+	public String print2() {
+		String result = "";
+		result = String.format("%s / %s / %s", 
+				this.getCourse_id(), this.getCourse_name(), (getCount_() > 0 ? 'X' : 'O'));
+		return result;
+	}
 }
