c2e07f6bc87a8a3f452eed59c68932891edb2b60
lhc9138 <lhc9138@naver.com>
2018-09-30 23:37:05 +0900

    Add method signature
    insert, print1, print2, remove

--- ScoreManagement/src/com/scoremanagement/persistance/SubjectDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/SubjectDAO.java
@@ -1,5 +1,39 @@
 package com.scoremanagement.persistance;
 
-public class SubjectDAO {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.scoremanagement.domain.Subject;
+
+public class SubjectDAO {
+	
+	// 과목 입력 메소드
+	public int insert(Subject s) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	// 과목 출력 메소드
+	// 과목번호 / 과목이름
+	public List<Subject> print1() {
+		List<Subject> list = new ArrayList<Subject>();
+		
+		return list;
+	}
+	
+	// 과목 출력 메소드
+	// 과목번호 / 과목이름 / 삭제가능여부
+	public List<Subject> print2() {
+		List<Subject> list = new ArrayList<Subject>();
+		
+		return list;
+	}
+	
+	// 과목 삭제 메소드
+	public int remove(Subject s) {
+		int result = 0;
+		
+		return result;
+	}
 }
