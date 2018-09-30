2833ba1efefabb1797405e0d6d04546f22425800
lhc9138 <lhc9138@naver.com>
2018-09-30 19:59:53 +0900

    Add method signature(login, modify)

--- ScoreManagement/src/com/scoremanagement/persistance/AdminDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/AdminDAO.java
@@ -1,5 +1,20 @@
 package com.scoremanagement.persistance;
 
-public class AdminDAO {
+import com.scoremanagement.domain.Admin;
 
+public class AdminDAO {
+	
+	// 관리자 로그인 메소드
+	public int login(Admin a) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	// 관리자 패스워드 변경 메소드
+	public int modify(Admin a) {
+		int result = 0;
+		
+		return result;
+	}
 }
