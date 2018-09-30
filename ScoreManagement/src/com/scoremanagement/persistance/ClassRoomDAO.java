1d90539b81732ee770a86af8b511fec916a7055a
lhc9138 <lhc9138@naver.com>
2018-09-30 22:18:19 +0900

    Add method signature
    insert, print1, print2, remove

--- ScoreManagement/src/com/scoremanagement/persistance/ClassRoomDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/ClassRoomDAO.java
@@ -1,6 +1,39 @@
 package com.scoremanagement.persistance;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.scoremanagement.domain.ClassRoom;
+
 public class ClassRoomDAO {
 
-
+	// 강의실 입력 메소드
+	public int insert(ClassRoom cr) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	// 강의실 출력메소드
+	// 강의실번호 / 강의실이름 / 최대정원
+	public List<ClassRoom> print1(ClassRoom cr) {
+		List<ClassRoom> list = new ArrayList<ClassRoom>();
+		
+		return list;
+	}
+	
+	// 강의실 출력메소드
+	// 강의실번호 / 강의실이름 / 최대정원 / 삭제가능여부
+	public List<ClassRoom> print2(ClassRoom cr) {
+		List<ClassRoom> list = new ArrayList<ClassRoom>();
+		
+		return list;
+	}
+	
+	// 강의실 삭제 메소드
+	public int remove(ClassRoom cr) {
+		int result = 0;
+		
+		return result;
+	}
 }
