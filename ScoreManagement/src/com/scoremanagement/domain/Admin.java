0487f5230581af2017502305e7bea58833f71a57
lhc9138 <lhc9138@naver.com>
2018-09-30 18:51:27 +0900

    Create Field, Constructor, Setter, Getter

--- ScoreManagement/src/com/scoremanagement/domain/Admin.java
+++ ScoreManagement/src/com/scoremanagement/domain/Admin.java
@@ -1,5 +1,35 @@
 package com.scoremanagement.domain;
 
 public class Admin {
+	// Field
+	private String admin_id;
+	private String admin_pw;
 
+	// Constructor
+	public Admin() {
+
+	}
+
+	public Admin(String admin_id, String admin_pw) {
+		this.admin_id = admin_id;
+		this.admin_pw = admin_pw;
+	}
+
+	// Getter
+	public String getAdmin_id() {
+		return admin_id;
+	}
+
+	public String getAdmin_pw() {
+		return admin_pw;
+	}
+
+	// Setter
+	public void setAdmin_id(String admin_id) {
+		this.admin_id = admin_id;
+	}
+
+	public void setAdmin_pw(String admin_pw) {
+		this.admin_pw = admin_pw;
+	}
 }
