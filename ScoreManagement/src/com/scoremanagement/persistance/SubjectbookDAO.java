14e14197b0f04bf1df07ae3b0e362107ff6cc092
lhc9138 <lhc9138@naver.com>
2018-09-30 23:41:07 +0900

    Add method signature
    insert, print1, print2, remove

--- ScoreManagement/src/com/scoremanagement/persistance/SubjectbookDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/SubjectbookDAO.java
@@ -1,5 +1,39 @@
 package com.scoremanagement.persistance;
 
-public class SubjectbookDAO {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.scoremanagement.domain.Subjectbook;
+
+public class SubjectbookDAO {
+	
+	// 교재 입력 메소드
+	public int insert(Subjectbook sb) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	// 교재 출력 메소드
+	// 교재번호 / 교재명 / 교재출판사
+	public List<Subjectbook> print1() {
+		List<Subjectbook> list = new ArrayList<Subjectbook>();
+		
+		return list;				
+	}
+	
+	// 교재 출력 메소드
+	// 교재번호 / 교재명 / 교재출판사 / 삭제가능여부
+	public List<Subjectbook> print2() {
+		List<Subjectbook> list = new ArrayList<Subjectbook>();
+		
+		return list;				
+	}
+	
+	// 교제 삭제 메소드
+	public int remove(Subjectbook sb) {
+		int result = 0;
+		
+		return result;
+	}
 }
