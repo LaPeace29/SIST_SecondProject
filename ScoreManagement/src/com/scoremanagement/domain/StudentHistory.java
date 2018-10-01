f2951227266a4ff7a848c657cc0a876278594a22
lhc9138 <lhc9138@naver.com>
2018-10-01 21:42:00 +0900

    Create Field In StudentHistory.java

--- ScoreManagement/src/com/scoremanagement/domain/StudentHistory.java
+++ ScoreManagement/src/com/scoremanagement/domain/StudentHistory.java
@@ -1,6 +1,11 @@
 package com.scoremanagement.domain;
 
+import java.sql.Date;
+
 public class StudentHistory {
 
-	
+	// Field
+	private String student_id;			// 수강생 아이디
+	private String open_course_id;		// 개설 과정 아이디
+	private Date dropout_date;			// 중도 탈락일
 }
