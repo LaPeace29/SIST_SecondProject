40866cd67973f792f8a31de2a38f1085f3561da3
lhc9138 <lhc9138@naver.com>
2018-10-02 00:26:03 +0900

    Add Field exam_file

--- ScoreManagement/src/com/scoremanagement/domain/Exam.java
+++ ScoreManagement/src/com/scoremanagement/domain/Exam.java
@@ -15,6 +15,7 @@ public class Exam {
 	private int write_point;			// 필기 배점
 	private int skill_point;			// 실기 배점
 	private Date exam_date;				// 시험 날짜
+	private String exam_file;			// 시험 문제 파일
 	private String student_id;			// 수강생 아이디
 	private String student_name;		// 수강생 이름
 	private String student_phone;		// 수강생 휴대폰번호
@@ -26,5 +27,7 @@ public class Exam {
 	private int class_count;			// 수강 횟수
 	private String completion_status;	// 수료 여부
 	private Date completion_date;		// 날짜(수료 or 중도탈락)
-	private String score_status;				// 성적 입력 여부(관리자 20번 참고)
+	private String score_status;		// 성적 입력 여부(관리자 20번 참고)
+
+
 }
