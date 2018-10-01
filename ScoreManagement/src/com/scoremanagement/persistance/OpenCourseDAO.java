bc38ecde767cab102fec74060956bc1d7a43f238
lhc9138 <lhc9138@naver.com>
2018-10-01 22:23:29 +0900

    Add method signature
    public int insert(OpenCourse oc)
    public List<OpenCourse> print1()
    public List<OpenCourse> print2()
    public List<OpenCourse> print3()
    public List<OpenCourse> print4()
    public List<OpenCourse> print5()
    public List<OpenCourse> print6()
    public List<OpenCourse> print7()
    public List<OpenCourse> search(String key, String value)
    public int remove(OpenCourse oc)

--- ScoreManagement/src/com/scoremanagement/persistance/OpenCourseDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/OpenCourseDAO.java
@@ -1,6 +1,86 @@
 package com.scoremanagement.persistance;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.scoremanagement.domain.OpenCourse;
+
 public class OpenCourseDAO {
 
+	// 개설 과정 추가 메소드
+	public int insert(OpenCourse oc) {
+		int result = 0;
+		
+		return result;
+	}
 	
+	// 개설 과정 출력 메소드(1)
+	// 개설 과정 번호 / 과정명 / 개설 과정 기간 / 강의실명
+	public List<OpenCourse> print1() {
+		List<OpenCourse> list = new ArrayList<OpenCourse>();
+		
+		return list;
+	}
+	
+	// 개설 과정 출력 메소드(2)
+	// 개설 과정 번호 / 과정명 / 개설 과정 기간 / 강의실명 / 개설 과목 등록 갯수 / 수강생 등록 인원
+	public List<OpenCourse> print2() {
+		List<OpenCourse> list = new ArrayList<OpenCourse>();
+		
+		return list;
+	}
+	
+	// 개설 과정 출력 메소드(3)
+	// 개설 과정 번호 / 과정명 / 개설 과정 기간 / 강의실명 / 삭제 가능 여부
+	public List<OpenCourse> print3() {
+		List<OpenCourse> list = new ArrayList<OpenCourse>();
+		
+		return list;
+	}
+	
+	// 개설 과정 출력 메소드(4)
+	// 개설 과정 번호 / 과정명 / 개설 과정 기간 / 강의실명 / 개설 과목명
+	public List<OpenCourse> print4() {
+		List<OpenCourse> list = new ArrayList<OpenCourse>();
+		
+		return list;
+	}
+	
+	// 개설 과정 출력 메소드(5)
+	// 개설 과정 번호 / 과정명 / 개설 과정 기간 / 강의실명 / 수료여부 / 날짜
+	public List<OpenCourse> print5() {
+		List<OpenCourse> list = new ArrayList<OpenCourse>();
+		
+		return list;
+	}
+	
+	// 개설 과정 출력 메소드(6)
+	// 과정명 / 개설 과정 기간 / 강의실명
+	public List<OpenCourse> print6() {
+		List<OpenCourse> list = new ArrayList<OpenCourse>();
+		
+		return list;
+	}
+	// 개설 과정 출력 메소드(7)
+	// 개설 과정 번호 / 과정명 / 개설과정기간
+	public List<OpenCourse> print7() {
+		List<OpenCourse> list = new ArrayList<OpenCourse>();
+		
+		return list;
+	}
+	
+	// 개설 과정 검색 메소드
+	// 1. 개설 과정 번호 2. 과정명
+	public List<OpenCourse> search(String key, String value) {
+		List<OpenCourse> list = new ArrayList<OpenCourse>();
+		
+		return list;
+	}
+	
+	// 개설 과정 삭제 메소드
+	public int remove(OpenCourse oc) {
+		int result = 0;
+		
+		return result;
+	}
 }
