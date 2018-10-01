e702a082df63e6c74cceec8727121e60fa2468ce
lhc9138 <lhc9138@naver.com>
2018-10-01 22:08:10 +0900

    Add method signature
    public int subjectPossibleAdd(InstructorPossible ip)
    public List<InstructorPossible> print(String instructor_id)
    public int subjectPossibleRemove(InstructorPossible ip)

--- ScoreManagement/src/com/scoremanagement/persistance/InsructorPossibleDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/InsructorPossibleDAO.java
@@ -1,5 +1,30 @@
 package com.scoremanagement.persistance;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.scoremanagement.domain.InstructorPossible;
+
 public class InsructorPossibleDAO {
 
+	// 강의 가능 과목 추가 메소드
+	public int subjectPossibleAdd(InstructorPossible ip) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	// 강의 가능 과목 출력 메소드
+	public List<InstructorPossible> print(String instructor_id) {
+		List<InstructorPossible> list = new ArrayList<InstructorPossible>();
+		
+		return list;
+	}
+	
+	// 강의 가능 과목 삭제 메소드
+	public int subjectPossibleRemove(InstructorPossible ip) {
+		int result = 0;
+		
+		return result;
+	}
 }
