76de2fad977cad38b7bc3529f756d4aec3a850fd
lhc9138 <lhc9138@naver.com>
2018-10-01 23:18:57 +0900

    Add method signature
    public List<Exam> print1()
    public List<Exam> print2()
    public List<Exam> print3()
    public List<Exam> print4()
    public List<Exam> print5()
    public List<Exam> print6()
    public int insertPoint(Exam e)
    public int removePoint(Exam e)
    public int insertScore(Exam e)
    public int removeScore(Exam e)

--- ScoreManagement/src/com/scoremanagement/persistance/ExamDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/ExamDAO.java
@@ -1,5 +1,85 @@
 package com.scoremanagement.persistance;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.scoremanagement.domain.Exam;
+
 public class ExamDAO {
 
+	// 시험 출력 메소드(1)
+	// 시험번호 / 출결 배점 / 필기 배점 / 실기 배점 / 시험 날짜 / 시험문제 파일
+	public List<Exam> print1() {
+		List<Exam> list = new ArrayList<Exam>();
+		
+		return list;
+	}
+	
+	// 시험 출력 메소드(2)
+	// 수강생 번호 / 수강생 이름 / 수강생 휴대폰번호 / 출결 점수 / 필기 점수 / 실기 점수 / 총점
+	public List<Exam> print2() {
+		List<Exam> list = new ArrayList<Exam>();
+		
+		return list;
+	}
+	
+	// 시험 출력 메소드(3)
+	// 과목명 / 개설 과목 기간 / 강사명 / 출결 점수 / 출결 배점 / 필기 점수 / 필기 배점 / 실기 점수 / 실기 배점 / 시험 날짜
+	public List<Exam> print3() {
+		List<Exam> list = new ArrayList<Exam>();
+		
+		return list;
+	}
+	
+	// 시험 출력 메소드(4)
+	// 시험번호 / 출결 배점 / 필기 배점 / 실기 배점 / 시험 날짜 / 시험문제 파일 / 성적 등록 인원수 / 성적 등록 여부
+	public List<Exam> print4() {
+		List<Exam> list = new ArrayList<Exam>();
+		
+		return list;
+	}
+	
+	// 시험 출력 메소드(5)
+	// 수강생 번호 / 수강생 이름 / 수강생 전화번호 / 수강생 등록일 / 수료여부 / 수료날짜 / 출결 점수 / 필기 점수 / 실기 점수 / 총점
+	public List<Exam> print5() {
+		List<Exam> list = new ArrayList<Exam>();
+		
+		return list;
+	}
+	
+	// 시험 출력 메소드(6)
+	// 교재명 / 강사명 / 출결 배점 / 필기 배점 / 실기 배점 / 출결 점수 / 필기 점수 / 실기 점수 / 시험 날짜 / 시험문제 파일
+	public List<Exam> print6() {
+		List<Exam> list = new ArrayList<Exam>();
+		
+		return list;
+	}
+	
+	// 시험 배점 입력 메소드
+	public int insertPoint(Exam e) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	// 시험 배점 삭제 메소드
+	public int removePoint(Exam e) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	// 시험 성적 입력 메소드
+	public int insertScore(Exam e) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	// 시험 성적 삭제 메소드
+	public int removeScore(Exam e) {
+		int result = 0;
+		
+		return result;
+	}
 }
