33bc5448fd2effe70f3028635a01c9d12765f76e
lhc9138 <lhc9138@naver.com>
2018-10-01 22:46:00 +0900

    Add method signature
    public int insert(OpenSubject os)
    public List<OpenSubject> print1
    public List<OpenSubject> print2
    public List<OpenSubject> print3
    public List<OpenSubject> print4
    public List<OpenSubject> print5
    public List<OpenSubject> print6
    public List<OpenSubject> print7
    public List<OpenSubject> print8
    public List<OpenSubject> print9
    public List<OpenSubject> search(String key, String value)
    public int remove(OpenSubject os)

--- ScoreManagement/src/com/scoremanagement/persistance/OpenSubjectDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/OpenSubjectDAO.java
@@ -1,5 +1,103 @@
 package com.scoremanagement.persistance;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.scoremanagement.domain.OpenSubject;
+
 public class OpenSubjectDAO {
 	
+	// 개설 과목 추가 메소드
+	public int insert(OpenSubject os) {
+		int result = 0;
+		
+		return result;
+	}
+	
+	// 개설 과목 출력 메소드(1)
+	// 개설 과목 번호 / 과목명 / 개설 과목 기간 / 교재명 / 강사명 / 과정명 / 개설 과정 기간 / 강의실명
+	public List<OpenSubject> print1() {
+		List<OpenSubject> list = new ArrayList<OpenSubject>();
+		
+		return list;
+	}
+	
+	// 개설 과목 출력 메소드(2)
+	// 개설 과목 번호 / 과목명 / 개설 과목 기간 / 교재명 / 강사명 / 과정명 / 개설 과정 기간 / 강의실명 / 삭제 가능 여부
+	public List<OpenSubject> print2() {
+		List<OpenSubject> list = new ArrayList<OpenSubject>();
+		
+		return list;
+	}
+	
+	// 개설 과목 출력 메소드(3)
+	// 과목명 / 개설 과목 기간
+	public List<OpenSubject> print3() {
+		List<OpenSubject> list = new ArrayList<OpenSubject>();
+		
+		return list;
+	}
+	
+	// 개설 과목 출력 메소드(4)
+	// 개설 과목 번호 / 과목명 / 개설 과목 기간 / 교재명 / 강사명
+	public List<OpenSubject> print4() {
+		List<OpenSubject> list = new ArrayList<OpenSubject>();
+		
+		return list;
+	}
+	
+	// 개설 과목 출력 메소드(5)
+	// 과목명 / 개설 과목 기간 / 교재명 / 강사명 / 과정명 / 개설 과정 기간 / 강의실명
+	public List<OpenSubject> print5() {
+		List<OpenSubject> list = new ArrayList<OpenSubject>();
+		
+		return list;
+	}
+	
+	// 개설 과목 출력 메소드(6)
+	// 개설 과목 번호 / 과목명 / 개설 과목 기간 / 비고
+	public List<OpenSubject> print6() {
+		List<OpenSubject> list = new ArrayList<OpenSubject>();
+		
+		return list;
+	}
+	
+	// 개설 과목 출력 메소드(7)
+	// 개설 과목 번호 / 과정명 / 개설 과정 기간 / 강의실명 / 과목명 / 개설 과목 기간 / 교재명 / 수강생 등록 인원
+	public List<OpenSubject> print7() {
+		List<OpenSubject> list = new ArrayList<OpenSubject>();
+		
+		return list;
+	}
+	
+	// 개설 과목 출력 메소드(8)
+	// 개설 과목 번호 / 과목명 / 개설 과목 기간
+	public List<OpenSubject> print8() {
+		List<OpenSubject> list = new ArrayList<OpenSubject>();
+		
+		return list;
+	}
+	
+	// 개설 과목 출력 메소드(9)
+	// 과목명 / 개설 과목 기간 / 과정명 / 개설 과정 기간 / 강의실 / 강의 진행 여부
+	public List<OpenSubject> print9() {
+		List<OpenSubject> list = new ArrayList<OpenSubject>();
+		
+		return list;
+	}
+
+	// 개설 과목 검색 메소드
+	// 1. 개설 과목 번호  2. 과목명
+	public List<OpenSubject> search(String key, String value) {
+		List<OpenSubject> list = new ArrayList<OpenSubject>();
+		
+		return list;
+	}
+	
+	// 개설 과목 삭제 메소드
+	public int remove(OpenSubject os) {
+		int result = 0;
+		
+		return result;
+	}
 }
