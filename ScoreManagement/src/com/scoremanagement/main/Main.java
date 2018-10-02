42a3bd5b76fffde54d12b166dbe9526e6dbbcd11
Lee <lhc9138@naver.com>
2018-10-02 14:47:50 +0900

    Insert main method And Create Scanner Instance

--- ScoreManagement/src/com/scoremanagement/main/Main.java
+++ ScoreManagement/src/com/scoremanagement/main/Main.java
@@ -1,8 +1,43 @@
 package com.scoremanagement.main;
 
+import java.util.Scanner;
+
+import com.scoremanagement.service.ServiceAdmin;
+import com.scoremanagement.service.ServiceInstructor;
+import com.scoremanagement.service.ServiceStudent;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("성적 처리 시스템 v6.0");
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("성적 처리 시스템 v6.0");
+		System.out.println("1. 수강생 로그인  2. 강사 로그인  3. 관리자 로그인");
+		System.out.print("선택 > ");
+		int selectNum = sc.nextInt();
+		
+		switch(selectNum) {
+		
+		case 1:
+			new ServiceStudent().login(sc);
+			break;
+			
+		case 2:
+			new ServiceInstructor().login(sc);
+			break;
+			
+		case 3:
+			new ServiceAdmin().login(sc);
+			break;
+			
+		default:
+			break;
+		}
+		
+		sc.close();
 	}
 }
