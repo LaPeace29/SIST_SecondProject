97cee7611047acf89666027465e69eb82ed27c86
Lee <lhc9138@naver.com>
2018-10-02 10:19:07 +0900

    Add method signature

--- ScoreManagement/src/com/scoremanagement/service/ServiceInstructor.java
+++ ScoreManagement/src/com/scoremanagement/service/ServiceInstructor.java
@@ -1,5 +1,76 @@
 package com.scoremanagement.service;
 
+import java.util.Scanner;
+
 public class ServiceInstructor {
 
+	// 성적 처리 시스템 v6.0 > 2. 강사 로그인
+	public void login(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : OOO) 
+	private void main(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : OOO) > 1. 강의 스케줄 조회
+	private void m1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : OOO) > 2. 배점 관리
+	private void m2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : 이상원) > 2. 배점 관리 > 1. 배점 입력
+	private void m2_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : 이상원) > 2. 배점 관리 > 2. 배점 출력
+	private void m2_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : 이상원) > 2. 배점 관리 > 3. 배점 삭제
+	private void m2_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : 이상원) > 3. 성적 관리
+	private void m3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : 이상원) > 3. 성적 관리 > 1. 성적 입력
+	private void m3_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : 이상원) > 3. 성적 관리 > 2. 성적 출력
+	private void m3_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : 이상원) > 3. 성적 관리 > 3. 성적 삭제
+	private void m3_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : 이상원) > 4. 개인 정보
+	private void m4(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : 이상원) > 4. 개인 정보 > 1. 개인 정보 조회
+	private void m4_s1() {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (강사 : 이상원) > 4. 개인 정보 > 2. 비밀 번호 변경
+	private void m4_s2(Scanner sc) {
+		
+	}
 }
