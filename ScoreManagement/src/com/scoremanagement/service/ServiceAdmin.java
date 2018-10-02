7214bd8f8de11f6b73a671e2b4ae70c839bebfcd
Lee <lhc9138@naver.com>
2018-10-02 11:53:38 +0900

    Add method signature

--- ScoreManagement/src/com/scoremanagement/service/ServiceAdmin.java
+++ ScoreManagement/src/com/scoremanagement/service/ServiceAdmin.java
@@ -1,5 +1,316 @@
 package com.scoremanagement.service;
 
+import java.util.Scanner;
+
 public class ServiceAdmin {
 
+	// 성적 처리 시스템 v6.0 > 3. 관리자 로그인
+	public void login(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) 
+	private void main(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 
+	private void m1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 1. 과정 관리
+	private void m1_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 1. 과정 관리 > 1. 과정 입력
+	private void m1_s1_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 1. 과정 관리 > 2. 과정 출력
+	private void m1_s1_s2() {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 1. 과정 관리 > 3. 과정 삭제
+	private void m1_s1_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 2. 과목 관리
+	private void m1_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 2. 과목 관리 > 1. 과목 입력
+	private void m1_s2_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 2. 과목 관리 > 2. 과목 출력
+	private void m1_s2_s2() {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 2. 과목 관리 > 3. 과목 삭제
+	private void m1_s2_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 3. 강의실 관리
+	private void m1_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 3. 강의실 관리 > 1. 강의실 입력
+	private void m1_s3_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 3. 강의실 관리 > 2. 강의실 출력
+	private void m1_s3_s2() {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 3. 강의실 관리 > 3. 강의실 삭제
+	private void m1_s3_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 4. 교재 관리
+	private void m1_s4(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 4. 교재 관리 > 1. 교재 입력
+	private void m1_s4_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 4. 교재 관리 > 2. 교재 출력
+	private void m1_s4_s2() {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 1. 기초 정보 관리 > 4. 교재 관리 > 3. 교재 삭제
+	private void m1_s4_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 2. 강사 계정 관리
+	private void m2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 2. 강사 계정 관리 > 1. 강사 입력
+	private void m2_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 2. 강사 계정 관리 > 2. 강사 검색 및 출력
+	private void m2_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 2. 강사 계정 관리 > 2. 강사 검색 및 출력 > 1. 강사 번호
+	private void m2_s2_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 2. 강사 계정 관리 > 2. 강사 검색 및 출력 > 2. 강사 이름
+	private void m2_s2_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 2. 강사 계정 관리 > 2. 강사 검색 및 출력 > 3. 강사 전체 출력
+	private void m2_s2_s3() {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 2. 강사 계정 관리 > 3. 강사 삭제
+	private void m2_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 2. 강사 계정 관리 > 4. 비밀번호 초기화
+	private void m2_s4(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 2. 강사 계정 관리 > 5. 강의 가능 과목 관리
+	private void m2_s5(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 2. 강사 계정 관리 > 5. 강의 가능 과목 관리 > 1. 강의 가능 과목 추가
+	private void m2_s5_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 2. 강사 계정 관리 > 5. 강의 가능 과목 관리 > 2. 강의 가능 과목 삭제
+	private void m2_s5_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 3. 개설 과정 관리
+	private void m3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 3. 개설 과정 관리 > 1. 개설 과정 추가
+	private void m3_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 3. 개설 과정 관리 > 2. 개설 과정 검색 및 출력
+	private void m3_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 3. 개설 과정 관리 > 2. 개설 과정 검색 및 출력 > 1. 개설 과정 번호
+	private void m3_s2_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 3. 개설 과정 관리 > 2. 개설 과정 검색 및 출력 > 2. 개설 과정명 
+	private void m3_s2_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 3. 개설 과정 관리 > 2. 개설 과정 검색 및 출력 > 3. 개설 과정 전체 출력
+	private void m3_s2_s3() {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 3. 개설 과정 관리 > 3. 개설 과정 삭제
+	private void m3_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 4. 개설 과목 관리
+	private void m4(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 4. 개설 과목 관리 > 1. 개설 과목 추가
+	private void m4_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 4. 개설 과목 관리 > 2. 개설 과목 검색 및 출력
+	private void m4_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 4. 개설 과목 관리 > 2. 개설 과목 검색 및 출력 > 1. 개설 과목 번호
+	private void m4_s2_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 4. 개설 과목 관리 > 2. 개설 과목 검색 및 출력 > 2. 개설 과목명
+	private void m4_s2_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 4. 개설 과목 관리 > 2. 개설 과목 검색 및 출력 > 3. 개설 과목 전체 출력
+	private void m4_s2_s3() {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 4. 개설 과목 관리 > 3. 개설 과목 삭제
+	private void m4_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리
+	private void m5(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리 > 1. 수강생 입력
+	private void m5_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리 > 2. 수강생 검색 및 출력
+	private void m5_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리 > 2. 수강생 검색 및 출력 > 1. 수강생 번호
+	private void m5_s2_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리 > 2. 수강생 검색 및 출력 > 2. 수강생 이름
+	private void m5_s2_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리 > 2. 수강생 검색 및 출력 > 3. 수강생 전체 출력
+	private void m5_s2_s3() {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리 > 3. 수강생 삭제
+	private void m5_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리 > 4. 비밀번호 초기화
+	private void m5_s4(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리 > 5. 수강생 과정 관리
+	private void m5_s5(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리 > 5. 수강생 과정 관리 > 1. 수강생 과정 등록
+	private void m5_s5_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리 > 5. 수강생 과정 관리 > 2. 수강생 과정 취소
+	private void m5_s5_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 5. 수강생 관리 > 5. 수강생 과정 관리 > 3. 수강생 중도 탈락
+	private void m5_s5_s3(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 6. 성적 조회
+	private void m6(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 6. 성적 조회 > 1. 개설 과목 성적 조회
+	private void m6_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 6. 성적 조회 > 2. 수강생 개인 성적 조회
+	private void m6_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 6. 성적 조회 > 2. 수강생 개인 성적 조회 > 1. 수강생 번호
+	private void m6_s2_s1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 6. 성적 조회 > 2. 수강생 개인 성적 조회 > 2. 수강생 이름
+	private void m6_s2_s2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (관리자 : admin) > 7. 비밀번호 변경
+	private void m7(Scanner sc) {
+		
+	}
 }
