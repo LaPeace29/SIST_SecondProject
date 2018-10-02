a628c105e2dda684d098b4d0e8f736f3ced2b16a
lhc9138 <lhc9138@naver.com>
2018-10-03 01:32:35 +0900

    Add method - print

--- ScoreManagement/src/com/scoremanagement/persistance/InsructorPossibleDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/InsructorPossibleDAO.java
@@ -1,8 +1,13 @@
 package com.scoremanagement.persistance;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.scoremanagement.connection.OracleConnection;
 import com.scoremanagement.domain.InstructorPossible;
 
 public class InsructorPossibleDAO {
@@ -17,6 +22,46 @@ public class InsructorPossibleDAO {
 	// 강의 가능 과목 출력 메소드
 	public List<InstructorPossible> print(String instructor_id) {
 		List<InstructorPossible> list = new ArrayList<InstructorPossible>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = OracleConnection.connect();
+			String sql = "SELECT s.subject_id, s.subject_name\r\n" + 
+					"    FROM instructor i, instructor_possible iso, subject s\r\n" + 
+					"    WHERE i.instructor_id = iso.instructor_id\r\n" + 
+					"    AND iso.subject_id = s.subject_id\r\n" + 
+					"    AND i.instructor_id = UPPER(?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, instructor_id);
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String subject_id = rs.getString("subject_id");
+				String subject_name = rs.getString("subject_name");
+				InstructorPossible ip = new InstructorPossible("", subject_id, subject_name);
+				list.add(ip);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+            try {
+                if (pstmt != null)
+                    pstmt.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+            try {
+                OracleConnection.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
 		
 		return list;
 	}
