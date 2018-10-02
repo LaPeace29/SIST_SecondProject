f110cfb9c6a5c9df2e670c209cd52fbceb324a26
Lee <lhc9138@naver.com>
2018-10-02 10:00:20 +0900

    Add method signature
    public void login(Scanner sc)
    private void main(Scanner sc)
    private void m1(Scanner sc)
    private void m1_s1()
    private void m2(Scanner sc)
    private void m2_s1()
    private void m2_s2(Scanner sc)

--- ScoreManagement/src/com/scoremanagement/service/ServiceStudent.java
+++ ScoreManagement/src/com/scoremanagement/service/ServiceStudent.java
@@ -1,5 +1,41 @@
 package com.scoremanagement.service;
 
+import java.util.Scanner;
+
 public class ServiceStudent {
 
+	// 성적 처리 시스템 v6.0 > 1. 수강생 로그인
+	public void login(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (수강생 : OOO) 
+	private void main(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (수강생 : OOO) > 1. 성적 조회
+	private void m1(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (수강생 : OOO) > 1. 성적 조회 > 개설 과정명
+	private void m1_s1() {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (수강생 : OOO) > 2. 개인 정보
+	private void m2(Scanner sc) {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (수강생 : OOO) > 2. 개인 정보 > 1. 개인 정보 조회
+	private void m2_s1() {
+		
+	}
+	
+	// 성적 처리 시스템 v6.0 (수강생 : OOO) > 2. 개인 정보 > 2. 비밀 번호 변경
+	private void m2_s2(Scanner sc) {
+		
+	}
 }
