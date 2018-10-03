e92b6933344ab7c690fae7d193a2de9eb5d4a9ec
lhc9138 <lhc9138@naver.com>
2018-10-04 00:07:06 +0900

    Add Field, Constructor, Getter, Setter
    Add - String admin_new_pw

--- ScoreManagement/src/com/scoremanagement/domain/Admin.java
+++ ScoreManagement/src/com/scoremanagement/domain/Admin.java
@@ -4,6 +4,7 @@ public class Admin {
 	// Field
 	private String admin_id;		// 관리자 아이디
 	private String admin_pw;		// 관리자 비밀번호
+	private String admin_new_pw;	// 관리자 새 비밀번호
 
 	// Constructor
 	public Admin() {
@@ -14,6 +15,12 @@ public class Admin {
 		this.admin_id = admin_id;
 		this.admin_pw = admin_pw;
 	}
+	
+	public Admin(String admin_id, String admin_pw, String admin_new_pw) {
+		this.admin_id = admin_id;
+		this.admin_pw = admin_pw;
+		this.admin_new_pw = admin_new_pw;
+	}
 
 	// Getter
 	public String getAdmin_id() {
@@ -23,6 +30,10 @@ public class Admin {
 	public String getAdmin_pw() {
 		return admin_pw;
 	}
+	
+	public String getAdmin_new_pw() {
+		return admin_new_pw;
+	}
 
 	// Setter
 	public void setAdmin_id(String admin_id) {
@@ -31,5 +42,9 @@ public class Admin {
 
 	public void setAdmin_pw(String admin_pw) {
 		this.admin_pw = admin_pw;
+	}
+	
+	public void setAdmin_new_pw(String admin_new_pw) {
+		this.admin_new_pw = admin_new_pw;
 	}
 }
