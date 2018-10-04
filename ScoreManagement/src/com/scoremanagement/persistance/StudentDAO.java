861b061a3528d659a8f8ea872102c43e9d16d639
lhc9138 <lhc9138@naver.com>
2018-10-05 00:16:58 +0900

    Move studentHistoryAdd method

--- ScoreManagement/src/com/scoremanagement/persistance/StudentDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/StudentDAO.java
@@ -67,43 +67,6 @@ public class StudentDAO {
 		return result;
 	}
 	
-	// 수강생 히스토리 추가 메소드
-	public int studentHistoryAdd(StudentHistory sh) {
-		int result = 0;
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		try {
-			conn = OracleConnection.connect();
-
-			String sql = "INSERT INTO student_history (open_course_id, student_id)\r\n"
-					+ "    VALUES (upper(?), upper(?))";
-
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, sh.getOpen_course_id());
-			pstmt.setString(2, sh.getStudent_id());
-
-			result = pstmt.executeUpdate();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (pstmt != null)
-					pstmt.close();
-			} catch (SQLException se) {
-				se.printStackTrace();
-			}
-			try {
-				OracleConnection.close();
-			} catch (SQLException se) {
-				se.printStackTrace();
-			}
-		}
-		
-		return result;
-	}
-	
 	// 수강생 출력 메소드(1)
 	// 수강생 이름 / 수강생 휴대폰번호
 	public List<Student> print1(String student_id) {
@@ -223,11 +186,21 @@ public class StudentDAO {
 			conn = OracleConnection.connect();
 			
 			if(key.equals("all")) {
-				String sql = "SELECT student_id, student_name, student_phone, student_regdate, count_\r\n" + 
+				String sql = "SELECT student_id, student_name, student_phone, student_regDate, count_\r\n" + 
 						"    FROM student_info_view1\r\n" + 
 						"        ORDER BY student_id";
 				
 				pstmt = conn.prepareStatement(sql);
+			}
+			
+			else if(key.equals("student_id")) {
+				String sql = "SELECT student_id, student_name, student_phone, student_regDate, count_\r\n" + 
+						"    FROM student_info_view1\r\n" + 
+						"    WHERE UPPER(student_id) = UPPER(?)\r\n" + 
+						"        ORDER BY student_id";
+				
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, value);
 			}
 
 			ResultSet rs = pstmt.executeQuery();
@@ -487,6 +460,43 @@ public class StudentDAO {
 		return result;
 	}
 	
+	// 수강생 히스토리 추가 메소드
+	public int studentHistoryAdd(StudentHistory sh) {
+		int result = 0;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = OracleConnection.connect();
+
+			String sql = "INSERT INTO student_history (open_course_id, student_id)\r\n"
+					+ "    VALUES (upper(?), upper(?))";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, sh.getOpen_course_id());
+			pstmt.setString(2, sh.getStudent_id());
+
+			result = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+			try {
+				OracleConnection.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+		
 	// 수강생 히스토리 삭제 메소드
 	public int studentHistoryRemove(StudentHistory sh) {
 		int result = 0;
