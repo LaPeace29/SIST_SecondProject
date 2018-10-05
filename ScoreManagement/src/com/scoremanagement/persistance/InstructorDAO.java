d629346dc4a71d55c280b68a8980eb3c6056f900
Lee <lhc9138@naver.com>
2018-10-05 15:10:29 +0900

    Add method print1

--- ScoreManagement/src/com/scoremanagement/persistance/InstructorDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/InstructorDAO.java
@@ -107,8 +107,47 @@ public class InstructorDAO {
 	
 	// 강사 출력 메소드(1)
 	// 강사이름 / 강사휴대폰번호
-	public List<Instructor> print1() {
+	public List<Instructor> print1(String instructor_id) {
 		List<Instructor> list = new ArrayList<Instructor>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = OracleConnection.connect();
+			String sql = "SELECT instructor_id, instructor_name, instructor_phone\r\n" + 
+					"    FROM instructor\r\n" + 
+					"    WHERE UPPER(instructor_id) = UPPER(?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, instructor_id);
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String instructor_id1 = rs.getString("instructor_id");
+				String instructor_name = rs.getString("instructor_name");
+				String instructor_phone = rs.getString("instructor_phone");
+				Instructor i = new Instructor(instructor_id1, instructor_name, instructor_phone, null);
+				list.add(i);
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+            try {
+                if (pstmt != null)
+                    pstmt.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+            try {
+                OracleConnection.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
 		
 		return list;
 	}
