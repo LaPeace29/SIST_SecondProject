6a7afe9d0e5229f5dfd43ca4edbd054c2d01f2bd
lhc9138 <lhc9138@naver.com>
2018-10-07 20:58:28 +0900

    Modify method name and remove method
    remove
    print1
    
    modify
    print2 -> list1
    print3 -> list2
    print4 -> list3

--- ScoreManagement/src/com/scoremanagement/persistance/InstructorDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/InstructorDAO.java
@@ -106,55 +106,8 @@ public class InstructorDAO {
 	}
 	
 	// 강사 출력 메소드(1)
-	// 강사이름 / 강사휴대폰번호
-	public List<Instructor> print1(String instructor_id) {
-		List<Instructor> list = new ArrayList<Instructor>();
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			conn = OracleConnection.connect();
-			String sql = "SELECT instructor_id, instructor_name, instructor_phone\r\n" + 
-					"    FROM instructor\r\n" + 
-					"    WHERE UPPER(instructor_id) = UPPER(?)";
-			
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, instructor_id);
-			
-			ResultSet rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				String instructor_id1 = rs.getString("instructor_id");
-				String instructor_name = rs.getString("instructor_name");
-				String instructor_phone = rs.getString("instructor_phone");
-				Instructor i = new Instructor(instructor_id1, instructor_name, instructor_phone, null);
-				list.add(i);
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-            try {
-                if (pstmt != null)
-                    pstmt.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-            try {
-                OracleConnection.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
-		
-		return list;
-	}
-	
-	// 강사 출력 메소드(2)
 	// 강사번호 / 강사이름 / 강사휴대폰번호 / 등록일
-	public List<Instructor> print2(String instructor_id) {
+	public List<Instructor> list1(String instructor_id) {
 		List<Instructor> list = new ArrayList<Instructor>();
 
 		Connection conn = null;
@@ -200,9 +153,9 @@ public class InstructorDAO {
 		return list;
 	}
 	
-	// 강사 출력 메소드(3)
+	// 강사 출력 메소드(2)
 	// 강사번호 / 강사이름 / 강사휴대폰번호 / 등록일 / 강의가능과목
-	public List<Instructor> print3(String key, String value) {
+	public List<Instructor> list2(String key, String value) {
 		List<Instructor> list = new ArrayList<Instructor>();
 		
 		Connection conn = null;
@@ -257,7 +210,7 @@ public class InstructorDAO {
 	
 	// 강사 출력 메소드(4)
 	// 강사번호 / 강사이름 / 강사휴대폰번호 / 등록일 / 삭제가능여부
-	public List<Instructor> print4() {
+	public List<Instructor> list3() {
 		List<Instructor> list = new ArrayList<Instructor>();
 		
 		Connection conn = null;
