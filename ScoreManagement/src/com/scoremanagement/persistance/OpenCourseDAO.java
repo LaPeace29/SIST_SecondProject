6587b22c86c7ef14ca5b2cbe2ad011c91e736e02
lhc9138 <lhc9138@naver.com>
2018-10-07 21:00:36 +0900

    Organize method...

--- ScoreManagement/src/com/scoremanagement/persistance/OpenCourseDAO.java
+++ ScoreManagement/src/com/scoremanagement/persistance/OpenCourseDAO.java
@@ -57,9 +57,20 @@ public class OpenCourseDAO {
 		return result;
 	}
 	
+	/*
+	CREATE OR REPLACE VIEW open_course_print1_view
+	AS
+	SELECT oc.open_course_id, c.course_name, oc.open_course_start_date, st.student_id
+    , oc.open_course_end_date, cr.class_room_name
+    FROM open_course oc, course c, class_room cr, student_history sh, student st
+    WHERE oc.course_id = c.course_id
+    AND oc.class_room_id = cr.class_room_id
+    AND oc.open_course_id = sh.open_course_id
+    AND sh.student_id = st.student_id;
+	 */
 	// 개설 과정 출력 메소드(1)
 	// 개설 과정 번호 / 과정명 / 개설 과정 기간 / 강의실명
-	public List<OpenCourse> print1(String open_course_id) {
+	public List<OpenCourse> print1(String key, OpenCourse value) {
 		List<OpenCourse> list = new ArrayList<OpenCourse>();
 		
 		Connection conn = null;
@@ -67,14 +78,29 @@ public class OpenCourseDAO {
 
 		try {
 			conn = OracleConnection.connect();
-			String sql = "SELECT open_course_id, course_name, open_course_start_date\r\n" + 
-					"    , open_course_end_date, class_room_name\r\n" + 
-					"    FROM open_course_search_view1\r\n"	+ 
-					"    WHERE UPPER(open_course_id) = UPPER(?)";
+			
+			if(key.equals("open_course_id")) {
+				String sql = "SELECT open_course_id, course_name, open_course_start_date\r\n" + 
+						"    , open_course_end_date, class_room_name\r\n" + 
+						"    FROM open_course_print1_view\r\n"	+ 
+						"    WHERE UPPER(open_course_id) = UPPER(?)";
 
-			pstmt = conn.prepareStatement(sql);
+				pstmt = conn.prepareStatement(sql);
 
-			pstmt.setString(1, open_course_id);
+				pstmt.setString(1, value.getOpen_course_id());
+			}
+			
+			else if(key.equals("student_id")) {
+				String sql = "SELECT open_course_id, course_name, open_course_start_date, open_course_end_date, class_room_name\r\n" + 
+						"    FROM open_course_print1_view\r\n" + 
+						"    WHERE UPPER(student_id) = UPPER(?)\r\n" + 
+						"    ORDER BY open_course_id";
+
+				pstmt = conn.prepareStatement(sql);
+
+				pstmt.setString(1, value.getStudent_id());
+			}
+			
 			ResultSet rs = pstmt.executeQuery();
 
 			while (rs.next()) {
@@ -107,7 +133,20 @@ public class OpenCourseDAO {
 		
 		return list;
 	}
-	
+	/*
+	CREATE OR REPLACE VIEW open_course_print2_view
+	AS
+    SELECT open_course_id, course_name, open_course_start_date
+                ,open_course_end_date, class_room_name
+                    ,(SELECT COUNT(*) FROM open_subject
+            WHERE open_course_id=oc.open_course_id) os_count
+            ,(SELECT COUNT(*) FROM student_history
+            WHERE open_course_id=oc.open_course_id) s_count
+            
+            FROM open_course oc, course c, class_room cr
+            WHERE oc.course_id = c.course_id
+            AND oc.class_room_id=cr.class_room_id;
+	 */
 	// 개설 과정 출력 메소드(2)
 	// 개설 과정 번호 / 과정명 / 개설 과정 기간 / 강의실명 / 개설 과목 등록 갯수 / 수강생 등록 인원
 	public List<OpenCourse> print2() {
@@ -119,10 +158,10 @@ public class OpenCourseDAO {
 		try {
 			conn = OracleConnection.connect();
 			String sql = "SELECT open_course_id, course_name, open_course_start_date\r\n" + 
-					"                ,open_course_end_date, class_room_name\r\n" + 
-					"                ,os_count, s_count\r\n" + 
-					"                FROM open_course_infoo1\r\n" + 
-					"            ORDER BY open_course_id";
+					"            ,open_course_end_date, class_room_name\r\n" + 
+					"            ,os_count, s_count\r\n" + 
+					"            FROM open_course_print2_view\r\n" + 
+					"        ORDER BY open_course_id";
 			
 			pstmt = conn.prepareStatement(sql);
 			
@@ -171,12 +210,12 @@ public class OpenCourseDAO {
 		
 		try {
 			conn = OracleConnection.connect();
-			String sql = "SELECT oci.open_course_id, course_name, open_course_start_date\r\n" + 
+			String sql = "SELECT v1.open_course_id, course_name, open_course_start_date\r\n" + 
 					"                ,open_course_end_date, class_room_name\r\n" + 
 					"                ,os_count, s_count,(SELECT COUNT(*) FROM student_history sh\r\n" + 
-					"                    WHERE sh.open_course_id=oci.open_course_id) count_\r\n" + 
-					"                FROM open_course_infoo1 oci\r\n" + 
-					"    ORDER BY open_course_id";
+					"                    WHERE sh.open_course_id=v1.open_course_id) count_\r\n" + 
+					"                FROM open_course_print2_view v1\r\n" + 
+					"    ORDER BY open_course_id;";
 			
 			pstmt = conn.prepareStatement(sql);
 			
@@ -214,6 +253,28 @@ public class OpenCourseDAO {
 		return list;
 	}
 	
+	/*
+		CREATE OR REPLACE VIEW open_course_print4_view1
+	AS    
+	SELECT cr.class_room_id, cr.class_room_name, cr.max_number, oc.open_course_id,
+	oc.open_course_start_date, oc.open_course_end_date, c.course_id, c.course_name
+    FROM open_course oc, course c, class_room cr
+    WHERE cr.class_room_id = oc.class_room_id
+    AND c.course_id = oc.course_id
+    AND sysdate < open_course_start_date;    
+    
+	CREATE OR REPLACE VIEW open_course_print4_view2
+	AS    
+	SELECT 
+	v1.open_course_id, v1.class_room_name, v1.course_name, v1.open_course_start_date, v1.open_course_end_date,
+	
+	(SELECT (LISTAGG(s.subject_name, ', ') WITHIN GROUP(ORDER BY s.subject_id))
+	    FROM open_subject t, subject s
+	   WHERE s.subject_id = t.subject_id 
+	   AND v1.open_course_id= t.open_course_id
+	 ) subjectlist
+    FROM open_course_print4_view1 v1;
+	 */
 	// 개설 과정 출력 메소드(4)
 	// 개설 과정 번호 / 과정명 / 개설 과정 기간 / 강의실명 / 개설 과목명
 	public List<OpenCourse> print4() {
@@ -227,7 +288,7 @@ public class OpenCourseDAO {
 			String sql = "SELECT open_course_id, course_name,\r\n" + 
 					"open_course_start_date, open_course_end_date, class_room_name,\r\n" + 
 					"subjectlist\r\n" + 
-					"    FROM open_course_add_list2\r\n" + 
+					"    FROM open_course_print4_view2\r\n" + 
 					"    ORDER BY open_course_id";
 			
 			pstmt = conn.prepareStatement(sql);
@@ -270,9 +331,32 @@ public class OpenCourseDAO {
 		return list;
 	}
 	
+	/*
+	CREATE OR REPLACE VIEW open_course_print5_view
+	AS
+	SELECT oc.open_course_id, course_name, open_course_start_date, open_course_end_date, 
+	class_room_name, sh.student_id, (SELECT dropout_date 
+                                        FROM process_complete pc 
+                                        WHERE pc.open_course_id = oc.open_course_id 
+                                        AND pc.student_id = sh.student_id) drop_date
+    FROM open_course oc, course c ,class_room cr, student_history sh
+    WHERE c.course_id = oc.course_id
+    AND cr.class_room_id = oc.class_room_id
+    AND oc.open_course_id = sh.open_course_id; 
+    
+	CREATE OR REPLACE VIEW open_course_print5_view2
+	AS
+	SELECT open_course_id, course_name, open_course_start_date, open_course_end_date, class_room_name,
+        CASE
+            WHEN SYSDATE < open_course_end_date AND drop_date is null THEN '수료예정'
+            WHEN drop_date IS NOT NULL THEN '중도탈락'
+            else '수료'
+        END completion, drop_date, student_id
+    FROM open_course_print5_view;
+	 */
 	// 개설 과정 출력 메소드(5)
 	// 개설 과정 번호 / 과정명 / 개설 과정 기간 / 강의실명 / 수료여부 / 날짜
-	public List<OpenCourse> print5(String key, String value1, String value2) {
+	public List<OpenCourse> print5(String key, OpenCourse value) {
 		List<OpenCourse> list = new ArrayList<OpenCourse>();
 		
 		Connection conn = null;
@@ -283,31 +367,31 @@ public class OpenCourseDAO {
 			
 			if(key.equals("student_id")) {
 				String sql = "SELECT open_course_id, course_name, open_course_start_date, open_course_end_date, class_room_name, completion, drop_date\r\n" + 
-						"    FROM s_course_search_detail_view2\r\n" + 
+						"    FROM open_course_print5_view2\r\n" + 
 						"    WHERE student_id = UPPER(?)";
 				
 				pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, value1);
+				pstmt.setString(1, value.getStudent_id());
 			}
 			
 			else if (key.equals("student_name")){
 				String sql = "SELECT open_course_id, course_name, open_course_start_date, open_course_end_date, class_room_name, completion, drop_date\r\n" + 
-						"    FROM s_course_search_detail_view2\r\n" + 
-						"    WHERE student_id = UPPER(?)";
+						"    FROM open_course_print5_view2\r\n" + 
+						"    WHERE INSTR(student_name, ?) > 0";
 				
 				pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, value1);
+				pstmt.setString(1, value.getStudent_name());
 			}
 			
-			else if (key.equals("open_course_id")) {
+			else if (key.equals("open_course_idANDstudent_id")) {
 				String sql = "SELECT open_course_id, course_name, open_course_start_date, open_course_end_date, class_room_name, completion, drop_date\r\n" + 
-						"    FROM s_course_search_detail_view2\r\n" + 
+						"    FROM open_course_print5_view2\r\n" + 
 						"    WHERE student_id = UPPER(?)\r\n" +
 						"    AND open_course_id = UPPER(?)";
 				
 				pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, value1);
-				pstmt.setString(2, value2);
+				pstmt.setString(1, value.getStudent_id());
+				pstmt.setString(2, value.getOpen_course_id());
 			}
 			ResultSet rs = pstmt.executeQuery();
 			
@@ -444,6 +528,15 @@ public class OpenCourseDAO {
 		return list;
 	}
 	
+	/*
+	CREATE OR REPLACE VIEW open_course_search_view1
+	AS
+	SELECT oc.open_course_id, c.course_name, oc.open_course_start_date
+    , oc.open_course_end_date, cr.class_room_name
+    FROM open_course oc, course c, class_room cr
+    WHERE oc.course_id = c.course_id
+    AND oc.class_room_id = cr.class_room_id;
+	 */
 	// 개설 과정 검색 메소드
 	// 1. 개설 과정 번호 2. 과정명
 	public List<OpenCourse> search(String key, OpenCourse value) {
@@ -468,7 +561,7 @@ public class OpenCourseDAO {
 			
 			else if(key.equals("course_name")) {
 				String sql = "SELECT open_course_id, course_name, open_course_start_date, open_course_end_date, class_room_name\r\n" + 
-						"    FROM c_info_view\r\n" + 
+						"    FROM open_course_search_view1\r\n" + 
 						"    WHERE INSTR(course_name, ?) > 0";
 				
 				pstmt = conn.prepareStatement(sql);
