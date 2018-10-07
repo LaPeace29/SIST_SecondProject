3967a07496012eeb6c696766bc3086be2c038e50
lhc9138 <lhc9138@naver.com>
2018-10-07 21:00:05 +0900

    Add Field - student_id
    Create method - print1, print2, print3, print4

--- ScoreManagement/src/com/scoremanagement/domain/OpenCourse.java
+++ ScoreManagement/src/com/scoremanagement/domain/OpenCourse.java
@@ -11,8 +11,10 @@ public class OpenCourse {
 	private String course_name;				// 과정 이름
 	private Date open_course_start_date;	// 개설 과정 시작일
 	private Date open_course_end_date;		// 개설 과정 종료일
-	private String subject_list;			// 개설 과목명(관리자 37번 참고)
+	private String subject_list;			// 개설 과목명
 	private int open_subject_count;			// 개설 과목 등록 갯수
+	private String student_id;				// 수강생 아이디
+	private String student_name;			// 수강생 이름
 	private int student_count;				// 수강생 등록 인원
 	private int count_;						// 삭제 가능 여부
 	private String completion_status;		// 수료 여부
@@ -24,14 +26,20 @@ public class OpenCourse {
 
 	}
 	
-	
 	// 개설 과정 삭제할 때 사용
-	// 개설 과정 검색 시 사용
 	public OpenCourse(String open_course_id, String course_name) {
 		this.open_course_id = open_course_id;
 		this.course_name = course_name;
 	}
 
+	// 개설 과목 검색시 사용
+	public OpenCourse(String open_course_id, String course_name, String student_id, String student_name) {
+		this.open_course_id = open_course_id;
+		this.course_name = course_name;
+		this.student_id = student_id;
+		this.student_name = student_name;
+	}
+	
 	// 개설 과정 추가할 때 사용
 	public OpenCourse(String open_course_id, String class_room_id, String course_id, Date open_course_start_date,
 			Date open_course_end_date) {
@@ -128,6 +136,14 @@ public class OpenCourse {
 		return open_subject_count;
 	}
 
+	public String getStudent_id() {
+		return student_id;
+	}
+
+	public String getStudent_name() {
+		return student_name;
+	}
+
 	public int getStudent_count() {
 		return student_count;
 	}
@@ -169,6 +185,14 @@ public class OpenCourse {
 		this.open_course_end_date = open_course_end_date;
 	}
 
+	public void setStudent_id(String student_id) {
+		this.student_id = student_id;
+	}
+
+	public void setStudent_name(String student_name) {
+		this.student_name = student_name;
+	}
+
 	public void setCompletion_status(String completion_status) {
 		this.completion_status = completion_status;
 	}
@@ -178,6 +202,39 @@ public class OpenCourse {
 	}
 	
 	// print method
+	public String print1() {
+		String result = "";
+		result = String.format("%s / %s / %s ~ %s / %s", 
+				this.getOpen_course_id(), this.getCourse_name(), 
+				this.getOpen_course_start_date(), this.getOpen_course_end_date(), this.getClass_room_name());
+		return result;
+	}
+
+	public String print2() {
+		String result = "";
+		result = String.format("%s / %s / %s ~ %s / %s / %d개 / %d명", 
+				this.getOpen_course_id(), this.getCourse_name(),
+				this.getOpen_course_start_date(), this.getOpen_course_end_date(), this.getClass_room_name(),
+				this.getOpen_subject_count(), this.getStudent_count());
+		return result;
+	}
+	
+	public String print3() {
+		String result = String.format("%s / %s / %s / %s ~ %s / %s\n", 
+				this.getOpen_course_id(), this.getClass_room_name(), this.getCourse_name(), 
+				this.getOpen_course_start_date(), this.getOpen_course_end_date(),
+				(this.getCount_() > 0 ? 'X' : 'O'));
+		return result;
+	}
+	
+	public String print4() {
+		String result = String.format("%s / %s / %s ~ %s / %s / %s",
+				this.getOpen_course_id(), this.getCourse_name(),
+				this.getOpen_course_start_date(), this.getOpen_course_end_date(),
+				this.getClass_room_name(), this.getSubject_list());
+		return result;
+	}
+	
 	public String print7() {
 		String result = "";
 		result = String.format("%s / %s / %s ~ %s", 
